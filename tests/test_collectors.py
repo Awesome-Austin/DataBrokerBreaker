da4a84a49db0ba6ad8bc63868c797c23a958d516
Awesome_Austin <{ID}+{username}@users.noreply.github.com>
2020-11-25 09:56:51 -0800

    added testing dir

---
+++ tests/test_collectors.py
@@ -0,0 +1,24 @@
+from collectors import COLLECTORS, MyLife
+from definitions import TEST_PERSON
+
+
+def test_all():
+    for collector in COLLECTORS:
+        with collector(TEST_PERSON, test=True) as c:
+            c.validate_data()
+            if c.check_relatives():
+                relatives = c.relatives
+                print(relatives)
+
+def test_mylife():
+
+    with MyLife(TEST_PERSON, test=True) as c:
+        c.validate_data()
+        if c.check_relatives():
+            relatives = c.relatives
+            print(relatives)
+
+
+if __name__ == '__main__':
+    test_all()
+    # test_mylife()
