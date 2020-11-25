86aacddf085b29553bac7c497fc3321d6e93cc85
Awesome_Austin <{ID}+{username}@users.noreply.github.com>
2020-11-25 09:57:22 -0800

    updated data output to match schema.org

--- collectors/__init__.py
+++ collectors/__init__.py
@@ -1,7 +1,9 @@
 from collectors.spokeo import Spokeo
 from collectors.mylife import MyLife
+from collectors.radaris import Radaris
 
 COLLECTORS = (
     Spokeo,
     MyLife,
+    Radaris,
 )
--- collectors/radaris/main.py
+++ collectors/radaris/main.py
@@ -1,4 +1,5 @@
 from urllib.parse import urljoin
+import logging
 
 import requests
 from bs4 import BeautifulSoup as bs
@@ -7,7 +8,12 @@ import pandas as pd
 from abstracts import RequestCollector, NoRecords
 from definitions import TEST_PERSON
 
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# logging.disable(logging.CRITICAL)
+
 BASE_URL = 'https://radaris.com/'
+
 
 
 class Radaris(RequestCollector):
@@ -15,15 +21,14 @@ class Radaris(RequestCollector):
     def __init__(self, person, **kwargs):
 
         super(Radaris, self).__init__(person, BASE_URL, **kwargs)
-        # self.person.state = STATES.get(self.person.state.upper(), self.person.state).title()
         self.url = urljoin(self.base_url, 'ng/search?{}'.format(
             '&'.join(
-                [s for s in ['{}'.format(f'ff={self.person.first_name}' if self.person.first_name is not None else ''),
-                             '{}'.format(f'fl={self.person.last_name}' if self.person.last_name is not None else ''),
-                             '{}'.format(f'fs={self.person.state}' if self.person.state is not None else ''),
-                             '{}'.format(
-                                 f'fc={self.person.city.replace(" ", "+")}' if self.person.city is not None else '')
-                             ] if len(s) > 0])))
+                [s for s in ['{}'.format(f'ff={self.person.get("givenName", "").replace(" ", "+")}'),
+                             '{}'.format(f'fl={self.person.get("familyName", "").replace(" ", "+")}'),
+                             '{}'.format(f'fs={self.person.get("addressRegion", "").replace(" ", "+")}'),
+                             '{}'.format(f'fc={self.person.get("addressLocality", "").replace(" ", "+")}')
+                             ] if len(s) > 0]
+            )))
 
     def __enter__(self):
         super(Radaris, self).__enter__()
@@ -37,33 +42,70 @@ class Radaris(RequestCollector):
         super(Radaris, self).__exit__(exc_type, exc_val, exc_tb)
 
     def get_data(self):
+        def _clean_data_(result):
+            id = result.find(itemprop="url").get('href').split('/')[-1]
+
+            given_name = result.find(itemprop='givenName')
+            given_name = given_name.text.strip() if given_name is not None else ''
+
+            middle_name = result.find(itemprop='additionalName')
+            middle_name = middle_name.text.strip() if middle_name is not None else ''
+
+            family_name = result.find(itemprop='familyName')
+            family_name = family_name.text.strip() if family_name is not None else ''
+
+            name_ = result.find(itemprop='name')
+            name_ = name_.text.strip() if name_ is not None else ''
+
+            age = result.find(class_='age')
+            age = age.text.replace('age:', '').strip() if age is not None else ''
+
+            address = {'@type': 'PostalAddress'}
+
+            try:
+                address['addressLocality'] = result.find(itemprop='AddressLocality').text.strip()
+            except AttributeError as e:
+                pass
+
+            try:
+                address['addressRegion'] = result.find(itemprop='AddressRegion').text.strip()
+            except AttributeError as e:
+                pass
+
+            addl_name = result.find(class_='ka')
+            addl_name = addl_name.text.split(":")[1].strip().split(', ') if addl_name is not None else ''
+
+            related_to = result.find_all(itemprop='relatedTo')
+            related_to = [r.find(itemprop='name').text.split(',')[0] for r in related_to if
+                          r.find(itemprop='name') is not None]
+
+            url = urljoin(self.base_url, result.find(itemprop="url").get('href'))
+
+            return {
+                '@context': 'http://schema.org',
+                '@type': 'Person',
+                'id': id,
+                'givenName': given_name,
+                'middleName': middle_name,
+                'familyName': family_name,
+                'name': name_,
+                'age': age,
+                'address': address,
+                'additionalName': addl_name,
+                'relatedTo': related_to,
+                'url': url
+            }
+
+        logging.debug(self.url)
         res = requests.get(self.url)
         res.raise_for_status()
         soup = bs(res.content, 'html.parser')
 
-        results = soup.find_all(lambda tag: tag.name =='div' and tag.get('itemtype') == 'http://schema.org/Person')
+        results = soup.find_all(lambda tag: tag.name == 'div' and tag.get('itemtype') == 'http://schema.org/Person')
 
-        df = []
-        for result in results:
-            df.append({
-                'id': result.find(itemprop="url").get('href').split('/')[-1],
-                'first_name': result.find(itemprop='givenName').text.strip(),
-                'middle_name': result.find(itemprop='additionalName').text.strip(),
-                'last_name': result.find(itemprop='familyName').text.strip(),
-                'name_': result.find(itemprop='name').text.strip(),
-                'age':  result.find(class_='age').text.replace('age:', '').strip(),
-                'city': {
-                    'city': result.find(itemprop='AddressLocality').text.strip(),
-                    'state': result.find(itemprop='AddressRegion').text.strip()
-                },
-                'aka': result.find(class_='ka').text.split(":")[1].strip().split(', '),
-                'relatedTo': [r.find(itemprop='name').text.split(',')[0] for r in
-                              result.find_all(itemprop='relatedTo')],
-                'url': urljoin(self.base_url, result.find(itemprop="url").get('href'))
-            })
-
-        self.df = pd.DataFrame(df)
-        self.df.set_index('id', inplace=True)
+        df = [_clean_data_(result) for result in results]
+        self.data_from_website = pd.DataFrame(df)
+        self.data_from_website.set_index('id', inplace=True)
 
     def validate_data(self):
         super(Radaris, self).validate_data()
