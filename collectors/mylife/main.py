9b07ef00f839e13d4c5d8da914f82b737dfceee8
Awesome_Austin <{ID}+{username}@users.noreply.github.com>
2020-11-25 09:57:22 -0800

    updated data output to match schema.org

--- collectors/mylife/main.py
+++ collectors/mylife/main.py
@@ -34,9 +34,13 @@ class MyLife(SeleniumCollector):
         :param person: Pandas.Series representing an individual
         """
         super(MyLife, self).__init__(person, BASE_URL, **kwargs)
-        self.url = urljoin(self.base_url, "pub-multisearch.pubview?search={first}+{last}".format(
-            first=self.person.first_name,
-            last=self.person.last_name))
+        self.url = urljoin(
+            self.base_url,
+            "pub-multisearch.pubview?search={first}+{last}".format(
+                first=self.person.get('givenName', ''),
+                last=self.person.get('familyName', ''))
+        )
+
         self.auto_scroll = kwargs.get('auto_scroll', True)
 
     def __enter__(self):
@@ -45,6 +49,7 @@ class MyLife(SeleniumCollector):
             self.get_data()
         except NoRecords:
             pass
+
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -73,45 +78,47 @@ class MyLife(SeleniumCollector):
             # Find all Addresses for search result.
 
             try:
-                top_city_states = search_hit.find(class_='hit-pastAddresses').find_all(class_='hit-values')
-                top_city_states = list({address.text.upper().replace('.', '') for address in top_city_states})
+                address = search_hit.find(class_='hit-pastAddresses').find_all(class_='hit-values')
+                address = list({a.text.upper().replace('.', '') for a in address})
             except AttributeError:
-                top_city_states = list()
+                address = list()
 
             # find the address that is most likely the current main address.
             try:
-                top_city_states_best_match_index = top_city_states.index(current_city)
-
+                address.insert(0, address.pop(address.index(current_city)))
             except ValueError:
-                top_city_states_best_match_index = len(top_city_states)
-                top_city_states.append(current_city)
+                address.insert(0, current_city)
 
-            top_city_states = [{'city': city_.title(), 'state': state_}
-                               for city_, state_ in [address.split(', ') for address in top_city_states]]
+            address = [
+                {
+                    '@type': 'PostalAddress',
+                    'addressLocality': address_locality.title(),
+                    'addressRegion': address_region
+                } for address_locality, address_region in [a.split(', ') for a in address]]
 
+            work_location = {'@type': 'Place'}
             try:
-                job = search_hit.find(class_='hit-work').find(class_='hit-values').get_text().title()
+                work_location['name'] = search_hit.find(class_='hit-work').find(class_='hit-values').get_text().title()
             except AttributeError:
-                job = ''
+                work_location['name'] = ''
 
+            alumni_of = {'@type': 'EducationalOrganization'}
             try:
-                school = search_hit.find(class_='hit-high-school').find(class_='hit-values').get_text().title()
+                alumni_of['name'] = search_hit.find(class_='hit-high-school').find(class_='hit-values').get_text().title()
             except AttributeError:
-                school = ''
+                pass
 
             return {
                 'id': hit_id,
-                'name_': ' '.join(name),
-                'main_name': {
-                    'first_name': name[0],
-                    'middle_name': ' '.join(name[1:-1]),
-                    'last_name': name[-1],
-                },
+                '@type': 'Person',
+                'name': ' '.join(name),
+                'givenName': name[0],
+                'middleName': ' '.join(name[1:-1]),
+                'familyName': name[-1],
                 'url': hit_url,
-                'top_city_states': top_city_states,
-                'top_city_states_best_match_index': top_city_states_best_match_index,
-                'work': job,
-                'school': school,
+                'address': address,
+                'workLocation': work_location,
+                'alumniOf': alumni_of,
             }
 
         def _refine_search(search_str, options):
@@ -151,21 +158,22 @@ class MyLife(SeleniumCollector):
             driver.fullscreen_window()
 
             # Refine the search by State
-            state = STATES.get(self.person.state.upper(), self.person.state.upper())
-            state_options = driver\
+            address_region = self.person.get('addressRegion', '')
+            address_region = STATES.get(address_region.upper(), address_region.upper())
+            region_options = driver\
                 .find_element_by_class_name("STATE")\
                 .find_elements_by_class_name("refinementList-text")
 
-            if not _refine_search(state, state_options):
+            if not _refine_search(address_region, region_options):
                 return False
 
             # Narrow the search by pressing a City option
-            city = self.person.city.title()
-            city_options = driver\
+            address_locality = self.person.get('addressLocality').title()
+            locality_options = driver\
                 .find_element_by_class_name("CITY")\
                 .find_elements_by_class_name("refinementList-text")
 
-            if not _refine_search(city, city_options):
+            if not _refine_search(address_locality, locality_options):
                 return False
 
             """
@@ -174,7 +182,6 @@ class MyLife(SeleniumCollector):
             Here it will first get the current location on the page, attempt to move down the page, and then check to
                 see if the location changed.
             """
-
 
             if self.auto_scroll and len(driver.find_elements_by_class_name("ais-InfiniteHits-item")) > 15:
                 current_height, new_height = 0, driver.execute_script("return document.body.scrollHeight")
@@ -209,11 +216,16 @@ class MyLife(SeleniumCollector):
         with self.driver(self.DRIVER_DIR) as driver:
             driver.get(url)
             driver.fullscreen_window()
+            time.sleep(2)
             txt = driver.page_source
 
         soup = bs(txt, 'html.parser')
 
-        profile_data = json.loads(soup.find(type="application/ld+json").string)
+        profile_data = soup.find(type="application/ld+json")
+        if profile_data is None:
+            self._raise_site_schema_change()
+        profile_data = profile_data.string
+        profile_data = json.loads(profile_data, strict=False)
         profile_data['id'] = profile_data.pop('@id').split('/')[-1]
 
         about = profile_data.pop('about')
@@ -224,61 +236,156 @@ class MyLife(SeleniumCollector):
         profile_data['name'] = name_
 
         name_ = name_.split()
-        profile_data['main_name'] = {
-            'givenName': name_[0],
-            'middle_name': ' '.join(name_[1:-1]),
-            'familyName':  name_[-1],
-        }
-        profile_data['homeLocation'] = [profile_data.pop('address')]
-        # profile_data['city'] = {
-        #     'city': profile_data['address']['addressLocality'],
-        #     'state': profile_data['address']['addressRegion']
-        # }
+        profile_data['givenName'] = name_[0]
+        profile_data['middleName'] = ' '.join(name_[1:-1])
+        profile_data['familyName'] = name_[-1]
 
-        profile_data['reputation_score'] = soup.find_all(class_='profile-score-display-number')[1].text
+        if soup.find(class_='rep-vcard-score') is not None:
+            profile_data['reputation_score'] = "{min}-{max}".format(
+                min=soup.find(class_='rep-vcard-min').text,
+                max=soup.find(class_='rep-vcard-max').text
+            )
 
-        profile_data['alert'] = {a.text for a in soup.find_all(class_='ln-flags-item')}
+        address = list()
+        address_ = soup.find_all(class_='card-address')
+        for a in address_:
+            street_address, locality_region_postal, *misc = [_.text for _ in a.find_all(class_='block-container')]
+            address_locality, locality_region_postal = locality_region_postal.split(',')
+            address_region, postal_code = locality_region_postal.split()
+            address.append({
+                'streetAddress': street_address,
+                'addressLocality': address_locality,
+                'addressRegion': address_region,
+                'postalCode': postal_code,
+            })
 
-        addresses = []
-        for a in soup.find(class_='contact-section-addresses').find_all('a', href=True):
-            u = a['href'].split('/')
-            if 'address' in u[3]:
-                street, city, state, postal = u[3:]
-                addresses.append({
-                    'street': street.replace('address-', '').replace("_", " "),
-                    'city': city,
-                    'state': state,
-                    'postal_code': postal,
-                    'url': a['href']
-                })
+        profile_data['address'] = address
 
-        profile_data['addresses'] = addresses
+        # phones = soup.find(class_='master-profile-card-wrapper-phone').find_all(class_='card-content')
+        # phones = [phone.text for phone in phones]
+        # profile_data['phone'] = phones
 
-        profile_data['aka'] = profile_data.pop('alternateName')
+        # emails = soup.find(class_='master-profile-card-wrapper-email').find_all(class_='card-content')
+        # emails = [email.text for email in emails]
+        # profile_data['email'] = emails
 
-        social = [
-            (s.text, s['href']) for s in
-            soup.find(class_='contact-section-socials').find_all('a', href=True)
-        ]
-        profile_data['social'] = social
+        personal_details = soup.find(class_='card-personal-details')
+        if personal_details is not None:
+            personal_details = personal_details.find_all(class_='item-container')
+            personal_details = [detail.text.split(': ') for detail in personal_details]
+            personal_details = {detail.lower().replace(' ', '_'): value for
+                                detail, value in personal_details if value != 'Add Info'}
 
-        relatives = soup.find_all(class_='relative-section-item-details')
-        profile_data['relatedTo'] = [{
-            'name': r.find('a').get_text().title(),
-            'url': urljoin(self.base_url, r.find('a').get('href')),
-            'city': r.find('span').get_text().split(', ')[0].title(),
-            'state': r.find('span').get_text().split(', ')[1].upper()
-        } for r in relatives]
+            birth_date = personal_details.pop('date_of_birth')
+            if len(birth_date) > 0:
+                profile_data['birthDate'] = birth_date
 
-        neighbors = soup.find_all(class_='neighbors-section-item-details')
-        profile_data['neighbors'] = [{
-            'name': n.find('a').get_text().title(),
-            'url': urljoin(self.base_url, n.find('a').get('href')),
-            'city': [a.get_text() for a in n.find_all('span', {'class': ''})],
-        } for n in neighbors]
+            for key_, value_ in personal_details.items():
+                profile_data[key_] = value_
 
-        pics = soup.find(class_='photo-section-items').find_all('img')
-        profile_data['pics'] = [p.get('data-src', None) for p in pics if p.get('data-src', None) is not None]
+
+        # Education
+        schools_ = soup.find(class_='card-education')
+        if schools_ is not None:
+            schools = list()
+            schools_ = schools_.find_all(class_='card-content')
+            for school in schools_:
+                school = [detail.text.split(': ') for detail in school.find_all(class_='item-container')]
+                school = {detail.lower().replace(' ', '_'): value for
+                          detail, value in school if value != 'Add Info'}
+
+                if len(school) == 0:
+                    continue
+
+                school['@type'] = 'EducationalOrganization'
+                school['name'] = school.pop('school')
+                school['streetAddress'], school['addressLocality'] = school.pop('city').split(', ')
+                schools.append(school)
+
+
+        # Work
+        employers = soup.find(class_='card-job')
+        if employers is not None:
+            works_for = list()
+            employers = employers.find_all(class_='card-content')
+            for employer in employers:
+                employer = [detail.text.split(': ') for detail in employer.find_all(class_='item-container')]
+                employer = {detail.lower().replace(' ', '_'): value for
+                            detail, value in employer if value != 'Add Info'}
+
+                if len(employer) == 0:
+                    continue
+
+                employer['@type'] = 'Organization'
+                try:
+                    employer['name'] = employer.pop('company')
+                except KeyError:
+                    continue
+
+                if len(employer.get('city', '')) > 0:
+                    employer['streetAddress'], employer['addressLocality'] = employer.pop('city').split(', ')
+
+                works_for.append(employer)
+
+            if len(works_for) > 0:
+                profile_data['worksFor'] = works_for
+
+        # Automobiles
+        automobiles = soup.find(class_='card-auto')
+        if automobiles is not None:
+            owns = list()
+            automobiles = automobiles.find_all(class_='card-content')
+            for automobile in automobiles:
+                automobile = [detail.text.split(': ') for detail in automobile.find_all(class_='item-container')]
+                automobile = {detail.lower().replace(' ', '_'): value for
+                              detail, value in automobile if value != 'Add Info'}
+
+                if len(automobile) == 0:
+                    continue
+
+                automobile['@type'] = 'Product'
+                automobile['model'] = ' '.join([automobile.pop('year'), automobile.pop('make'), automobile.pop('model')])
+                owns.append(automobile)
+
+            if len(owns) > 0:
+                profile_data['owns'] = owns
+
+        related_to = list()
+        relatives = soup.find_all(class_='relative-container')
+        for relative in relatives:
+            relative_ = [r.text.split(', ') for r in relative.find_all(class_='default-text')]
+            relative = {'name': relative_[0][0].title()}
+
+            if len(relative_[0]) == 2:
+                relative['age'] = relative_[0][1]
+
+            if len(relative_[1]) > 0:
+                relative['addressLocality'] = relative_[1][0].title()
+                if len(relative_[1]) == 2:
+                    relative['addressRegion'] = relative_[1][1].upper()
+
+            related_to.append(relative)
+
+        if len(related_to) > 0:
+            profile_data['relatedTo'] = related_to
+
+        neighbors = list()
+        neighbors_ = soup.find_all(class_='neighbor-container')
+        for neighbor_ in neighbors_:
+            neighbor_ = [r.text.split(', ') for r in neighbor_.find_all(class_='default-text')]
+            neighbor = {'name': neighbor_[0][0].title()}
+            if len(neighbor_[0]) == 2:
+                neighbor['age'] = neighbor_[0][1]
+
+            if len(neighbor_[1]) > 0:
+                neighbor['addressLocality'] = neighbor_[1][0].title()
+                if len(neighbor_[1]) == 2:
+                    neighbor['addressRegion'] = neighbor_[1][1].upper()
+
+            neighbors.append(neighbor)
+
+        if len(neighbors) > 0:
+            profile_data['neighbors'] = neighbors
 
         return profile_data
 
@@ -303,7 +410,7 @@ class MyLife(SeleniumCollector):
         super(MyLife, self).validate_data()
         self._gather_deep_data()
         for i, record in self.data_from_website.iterrows():
-            for pic in record.pics:
+            for pic in record.get('pics', list()):
                 self.download_file(pic, f'{record.id}_{i}')
 
 
@@ -314,5 +421,5 @@ if __name__ == '__main__':
     #         relatives = ml.relatives()
 
     ml = MyLife(TEST_PERSON, test=True)
-    dd = ml._deep_data('https://www.mylife.com/john-smith/e781306924602')
+    dd = ml._deep_data('https://www.mylife.com/john-smith/e781501602096')
     print()
